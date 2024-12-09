f8a29e4ac77d462219824b1381ea73b7f98d39f5
Bohdan Horb <wannaalwaysbe@gmail.com>
2024-12-09 20:59:23 +0200

    Add sql connection

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 require('dotenv').config();
-
-const express = require('express')
+const express = require('express');
+const mysql = require('mysql2/promise');
 
 const PORT = process.env.PORT || 8080;
 
-const app = express()
+const app = express();
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST || '127.0.0.1',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+});
 
 app.get('/', (req, res) => {
     res.status(200).json({
         message: 'Hello, world!',
     });
+});
+
+app.get('/connection', async (req, res) => {
+    try {
+        const connection = await pool.getConnection();
+        await connection.ping();
+        connection.release();
+
+        res.status(200).json({
+            status: 'success',
+            message: 'Database connection is established.',
+        });
+    } catch (err) {
+        console.error(`[server] Error connecting to the database: ${err.message}`);
+        res.status(500).json({
+            status: 'error',
+            message: 'Unable to connect to the database.',
+            error: err.message,
+        });
+    }
 });
 
 (async () => {
